38e4a4bd41549b3be0bfbba7285ee98b8c5b3e70
Rich O'Reilly <roreilly@charitybuzz.com>
2012-12-29 17:51:12 -0500

    master removed generic theme

--- wp-content/themes/quickstep/post-meta.php
+++
@@ -1,5 +0,0 @@
-    <div class="meta">
-        <time datetime="<?php echo the_time('Y-m-j'); ?>" ><span class="day"><?php the_time('j'); ?></span><span class="month"><?php the_time('M'); ?> '<?php the_time('y'); ?></span></time>
-        <span class="post-author"><?php _e("By", "qs_framework"); ?> <?php the_author_posts_link(); ?> </span>
-        <?php the_tags('<p class="tags">', '<br />', '</p>'); ?>
-    </div>	
